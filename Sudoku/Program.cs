f92b176bac9188fabdaf3da3d3daa844747dc345
Niklas Hedlund <nojan1989@gmail.com>
2018-01-22 10:34:45 +0100

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    class SolverDataCombo
+    {
+        public SudokuSolver Solver { get; set; }
+        public string Solution { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var digits = File.ReadAllText("Testfiles/real.txt").Select(x => Convert.ToInt32(x.ToString()));
+            //var sudokuSolver = new SudokuSolver(digits);
+
+            //var board = sudokuSolver.Solve();
+
+            var tasks = File.ReadAllLines("Testfiles/a-lot-of-sudokus.txt")
+                .Select(line =>{
+                    var solution = line.Substring(0, line.IndexOf(';'));
+                    var problem = line.Substring(line.IndexOf(';') + 1);
+
+                    return new SolverDataCombo
+                    {
+                        Solver = new SudokuSolver(problem.Select(x => Convert.ToInt32(x.ToString()))),
+                        Solution = solution
+                    };
+                })
+                .ToList();
+
+
+            var stopWatch = new Stopwatch();
+            int numSolved = 0;
+
+            stopWatch.Start();
+
+            foreach(var task in tasks)
+            {
+                task.Solver.Solve();
+
+                numSolved++;
+
+                if (numSolved % 50 == 0)
+                    PrintStat(numSolved, stopWatch);
+            }
+
+            stopWatch.Stop();
+            PrintStat(numSolved, stopWatch);
+
+            Console.ReadKey();
+
+            for(int i = 0; i < tasks.Count; i++)
+            {
+                var calculatedSolution = ReduceToString(tasks[i].Solver.Board);
+                if(calculatedSolution != tasks[i].Solution)
+                {
+                    Console.WriteLine("Missmatch detected! At line {i+1}");
+                    Console.WriteLine($"Got: {calculatedSolution}");
+                    Console.WriteLine($"Expected: {tasks[i].Solution}");
+                    Console.WriteLine("=================================");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static string ReduceToString(int[,] board)
+        {
+            var sb = new StringBuilder();
+
+            for(int y = 0; y < board.GetLength(1); y++)
+            {
+                for(int x = 0; x < board.GetLength(0); x++)
+                {
+                    sb.Append(board[x, y]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        static void PrintStat(int numSolved, Stopwatch stopWatch)
+        {
+            double rate = (double)numSolved / stopWatch.Elapsed.TotalSeconds;
+
+            Console.Clear();
+            Console.WriteLine($"Sudokus solved: {numSolved}");
+            Console.WriteLine($"Time elapsed: {stopWatch.Elapsed.TotalSeconds} seconds");
+            Console.WriteLine($"Rate: {rate} sudoku/sec");
+
+            Console.WriteLine(stopWatch.IsRunning ? "Running...." : "Done");
+        }
+
+        static void Print(int[,] board, SudokuSolver sudokuSolver)
+        {
+            for (int y = 0; y < SudokuSolver.SIDE; y++)
+            {
+                Console.Write("|");
+                for (int x = 0; x < SudokuSolver.SIDE; x++)
+                {
+                    if (y % 3 == 0)
+                        Console.Write("====");
+                    else
+                        Console.Write("----");
+                }
+                Console.WriteLine("\b|");
+
+                for (int x = 0; x < SudokuSolver.SIDE; x++)
+                {
+                    if (x % 3 == 0)
+                        Console.Write("| ");
+                    else
+                        Console.Write(": ");
+
+                    Console.Write($"{board[x, y]} ");
+                }
+                Console.WriteLine("|");
+            }
+
+            Console.Write("|");
+            for (int x = 0; x < SudokuSolver.SIDE; x++)
+                Console.Write("====");
+
+            Console.WriteLine("\b|");
+
+
+            Console.Write($"Num backtracks: {sudokuSolver.NumBacktracks}");
+        }
+
+    }
+}
---
+++ Sudoku/SudokuSolver.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku
+{
+    public class SudokuSolver
+    {
+        public const int SIDE = 3 * 3;
+
+        public int NumBacktracks { get; private set; }
+        public int[,] Board { get; private set; }
+
+        public SudokuSolver(IEnumerable<int> digits)
+        {
+            Board = new int[SIDE, SIDE];
+            int i = 0;
+
+            foreach(var digit in digits)
+            {
+                int x = i % SIDE;
+                int y = i++ / SIDE;
+
+                Board[x, y] = digit;
+
+                if (i > (SIDE * SIDE) - 1)
+                    break;
+            }
+        }
+
+        public SudokuSolver(int[,] board)
+        {
+            if (board.GetLength(0) != SIDE || board.GetLength(1) != SIDE)
+                throw new ArgumentException("Board is not the correct size");
+
+            Board = board;
+        }
+
+        public int[,] Solve()
+        {
+            NumBacktracks = 0;
+            SolveImpl(Board, 0);
+
+            return Board;
+        }
+
+        private bool SolveImpl(int[,] board, int index)
+        {
+            if (index > (SIDE * SIDE) - 1)
+                return true;
+
+            //Convert from linear to grid coordinates
+            int x = index % SIDE;
+            int y = index / SIDE;
+
+            if (Board[x,y] != 0)
+                return SolveImpl(board, index + 1);
+
+            //Test each possible value
+            for (int i = 1; i <= 9; i++)
+            {
+                if (IsValid(board, i, x, y))
+                {
+                    board[x,y] = i;
+
+                    var result = SolveImpl(board, index + 1);
+                    if (result)
+                        return true;
+
+                    //Erase out the flawed solution
+                    board[x,y] = 0;
+                    NumBacktracks++;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsValid(int[,] board, int toTest, int x, int y)
+        {
+            //Find that quadrant start coordinates
+            int qX = (x / 3) * 3;
+            int qY = (y / 3) * 3;
+
+            //Test local
+            for (int lx = 0; lx < 3; lx++)
+            {
+                for (int ly = 0; ly < 3; ly++)
+                {
+                    if (board[qX + lx, qY + ly] == toTest)
+                        return false;
+                }
+            }
+
+            //Test global horizontal
+            for (int gx = 0; gx < SIDE; gx++)
+            {
+                if (board[gx, y] == toTest)
+                    return false;
+            }
+
+            //Test global vertical
+            for (int gy = 0; gy < SIDE; gy++)
+            {
+                if (board[x, gy] == toTest)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
